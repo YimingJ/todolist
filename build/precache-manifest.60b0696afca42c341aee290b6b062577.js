7c6161747913d4cc48ebaf75e5566f6615808576
YimingJ <jiangyimings@gmail.com>
2018-10-24 17:31:55 +0800

    add build

---
+++ build/precache-manifest.60b0696afca42c341aee290b6b062577.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c279da7851a8e1678517",
+    "url": "./static/js/main.c279da78.chunk.js"
+  },
+  {
+    "revision": "ed22e71205f96ac9c031",
+    "url": "./static/js/1.ed22e712.chunk.js"
+  },
+  {
+    "revision": "c279da7851a8e1678517",
+    "url": "./static/css/main.faeca526.chunk.css"
+  },
+  {
+    "revision": "8c61727279a9b0cbb919642b5e983def",
+    "url": "./index.html"
+  }
+];
